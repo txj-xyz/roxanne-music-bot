69f68c079d7757b55348e583ff0d4ab34d240743
txj-xyz <jfinelli3@gmail.com>
2021-11-30 13:35:38 -0700

    dev: patch broken messages

--- src/modules/ShoukakuHandler.js
+++ src/modules/ShoukakuHandler.js
@@ -8,7 +8,7 @@ class ShoukakuHandler extends Shoukaku {
         this.on('ready',
             (name, resumed) => {
                 client.logger.log(`Lavalink Node: ${name} is now connected`, `This connection is ${resumed ? 'resumed' : 'a new connection'}`)
-                client.webhook.send(`Lavalink Node: ${name} is now connected`, `This connection is ${resumed ? 'resumed' : 'a new connection'}`)
+                client.webhook.send(`Lavalink Node: ${name} is now connected This connection is ${resumed ? 'resumed' : 'a new connection'}`)
             }
         );
         this.on('error',
@@ -20,13 +20,13 @@ class ShoukakuHandler extends Shoukaku {
         this.on('close',
             (name, code, reason) => {
                 client.logger.log(`Lavalink Node: ${name} closed with code ${code}`, reason || 'No reason')
-                client.webhook.send(`Lavalink Node: ${name} closed with code ${code}`, reason || 'No reason')
+                client.webhook.send(`Lavalink Node: ${name} closed with code ${code} ${reason || 'No reason'}`)
             }
         );
         this.on('disconnect',
             (name, players, moved) => {
                 client.logger.log(`Lavalink Node: ${name} disconnected`, moved ? 'players have been moved' : 'players have been disconnected')
-                client.webhook.send(`Lavalink Node: ${name} disconnected`, moved ? 'players have been moved' : 'players have been disconnected')
+                client.webhook.send(`Lavalink Node: ${name} disconnected ${moved ? 'players have been moved' : 'players have been disconnected'}`)
             }
         );
         // this.on('debug',
