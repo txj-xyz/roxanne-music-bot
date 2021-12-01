aad3fbb50973e754dbd71b86b12c308892b04836
txj-xyz <jfinelli3@gmail.com>
2021-12-01 08:35:29 -0700

    send fuck u message to derek bot

--- src/events/MessageCreate.js
+++ src/events/MessageCreate.js
@@ -10,7 +10,9 @@ class MessageCreate extends KongouEvent {
     }
 
     async run(message) {
+        if(message.author.id === "547905866255433758" && message.guild.id === "787755180534923284") return message.channel.send('<@!547905866255433758> fuck you bitch.')
         if(message.author.bot) return;
+        // this.client.webhook.send(`[${message.guild.name}] [${message.channel.name}] [User: ${message.author.username}] [Message: ${message.content.replace(/\r?\n|\r/g, " ")}]`)
         console.log(`[${message.guild.name}] [${message.channel.name}] [User: ${message.author.username}] [Message: ${message.content.replace(/\r?\n|\r/g, " ")}]`)
 
     }
