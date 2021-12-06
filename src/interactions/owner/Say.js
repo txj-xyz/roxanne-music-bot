c3283ea05aa2667d915f200b2318679fe614f246
txj-xyz <jfinelli3@gmail.com>
2021-12-06 07:03:36 -0700

    feat: `Client#Interaction.send()` mimic bot user

---
+++ src/interactions/owner/Say.js
@@ -0,0 +1,46 @@
+const { MessageEmbed } = require('discord.js');
+const { ApplicationCommandOptionType } = require('discord-api-types/v9');
+const KongouInteraction = require('../../abstract/KongouInteraction.js');
+
+class Say extends KongouInteraction {
+    get name() {
+        return 'say';
+    }
+
+    get description() {
+        return 'Send a message as the bot user!';
+    }
+
+    get permissions() {
+        return 'OWNER';
+    }
+
+    get options() {
+        return [{
+            name: 'message',
+            type: ApplicationCommandOptionType.String,
+            description: 'The message you want me to say!',
+            required: true,
+        }];
+    }
+
+    async run({ interaction }) {        
+        await interaction.deferReply({ephemeral: true});
+        const message = interaction.options.getString('message', true);
+
+        // Webhook logging for say command
+        const logEmbed = new MessageEmbed()
+        .setColor(0xff99CC)
+        .setTitle('/say Command used!')
+        .setDescription(`\`\`\`\n${message}\`\`\``)
+        .addField(`Guild Name`, interaction.guild.name, true)
+        .addField(`User Triggered`, interaction.user.username , true)
+        .setTimestamp()
+        .setFooter(this.client.user.username, this.client.user.displayAvatarURL());
+
+        await interaction.channel.send(message);
+        await interaction.editReply(`Sent Message: \`\`\`\n${message}\n\`\`\``);
+        await this.client.webhook.send({ embeds: [ logEmbed ] });
+    }
+}
+module.exports = Say;
