c54b2f77ed918830c76dc6afb41f931cc0981d6c
txj-xyz <jfinelli3@gmail.com>
2021-12-06 15:08:46 -0700

    feat: redis db handler for stats

---
+++ src/modules/DatabaseHandler.js
@@ -0,0 +1,18 @@
+const { createClient } = require('redis');
+
+class DatabaseHandler {
+    constructor(client) {
+        this.redis = null;
+        this.redis = createClient({url: 'redis://192.168.1.20:31181'});
+        this.redis.connect().catch(err => {
+            this.redis = "Not Connected";
+            client.logger.debug(this.constructor.name, err)
+            return this.redis;
+        });
+        client.logger.debug(this.constructor.name, `Loaded Database Handler without Error.`);
+        return this;
+    }
+    
+}
+
+module.exports = DatabaseHandler;
