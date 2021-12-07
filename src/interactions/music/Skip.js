105fad74af30c604a8f7cb1b1607b9d81c9d1c08
txj-xyz <jfinelli3@gmail.com>
2021-12-06 18:10:51 -0700

    feat: optional `to` skip to track id in queue

--- src/interactions/music/Skip.js
+++ src/interactions/music/Skip.js
@@ -1,4 +1,6 @@
 const RoxanneInteraction = require('../../abstract/RoxanneInteraction.js');
+const { ApplicationCommandOptionType } = require('discord-api-types/v9');
+let tempQueue;
 
 class Skip extends RoxanneInteraction {
     get name() {
@@ -13,9 +15,30 @@ class Skip extends RoxanneInteraction {
         return { voice: true, dispatcher: true, channel: true };
     }
 
+    get options() {
+        return [{
+            name: 'to',
+            type: ApplicationCommandOptionType.String,
+            description: `Queue number to skip player ahead to.`,
+            required: false
+        }];
+    }
+
     async run({ interaction, dispatcher }) {
-        await interaction.reply('Skipping the currently playing track!');
-        dispatcher.player.stopTrack();
+        await interaction.deferReply();
+        tempQueue = dispatcher.queue;
+        const skipVariable = interaction.options.getString('to', false);
+
+        if(Number(skipVariable)) {
+            dispatcher.queue = dispatcher.queue.splice(Number(skipVariable) - 1, tempQueue.length);
+            tempQueue = null;
+            await interaction.editReply(`Skipping ${skipVariable} songs in the queue!`);
+            return dispatcher.player.stopTrack();
+        } else {
+            await interaction.editReply('Skipping the currently playing track!');
+            dispatcher.player.stopTrack();
+        }
+        
     }
 }
 module.exports = Skip;
