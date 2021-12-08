aefbb09bd79b0e99ee4a534b89b39e561469fa10
txj-xyz <jfinelli3@gmail.com>
2021-12-08 07:21:45 -0700

    feat: Queue pages!

--- src/interactions/music/Queue.js
+++ src/interactions/music/Queue.js
@@ -1,7 +1,8 @@
 
 const RoxanneInteraction = require('../../abstract/RoxanneInteraction.js');
 const RoxanneDispatcher = require('../../modules/RoxanneDispatcher.js');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageButton } = require('discord.js');
+const { PagesBuilder } = require('discord.js-pages');
 
 class Queue extends RoxanneInteraction {
     get name() {
@@ -16,20 +17,87 @@ class Queue extends RoxanneInteraction {
         return { voice: false, dispatcher: true, channel: false };
     }
 
+    static pageButtonList = [
+        {
+            back: new MessageButton()
+                .setLabel('Previous')
+                .setStyle('DANGER')
+        },
+        {
+            stop: new MessageButton()
+                .setLabel('Cancel')
+                .setStyle('DANGER')
+        },
+        {
+            next: new MessageButton()
+                .setLabel('Next')
+                .setStyle('SUCCESS')
+        }
+    ];
+
+    static chunkify(arr, len) {
+        let chunks = [];
+        let i = 0;
+        let n = arr.length;
+        
+        while (i < n) {
+            chunks.push(arr.slice(i, (i += len)));
+        }
+        
+        return chunks;
+    }
+
     async run({ interaction, dispatcher }) {
-        const queue = dispatcher.queue.length > 10 ? dispatcher.queue.slice(0, 10) : dispatcher.queue;
-        const embed = new MessageEmbed()
-            .setAuthor(`Now Playing`, this.client.user.displayAvatarURL({ dynamic: true }))
-            .setThumbnail(`https://img.youtube.com/vi/${dispatcher.current.info.identifier}/default.jpg`)
-            .setURL(dispatcher.current.info.uri)
-            .setColor(this.client.color)
-            .setTitle(`**${dispatcher.current.info.title}**`)
-            .addField(`⌛ Duration: `, `\`${RoxanneDispatcher.humanizeTime(dispatcher.current.info.length)}\``, true)
-            .addField(`🎵 Author: `, `\`${dispatcher.current.info.author}\``, true)
-            .setFooter(`• ${dispatcher.queue.length} total songs in queue`)
-            .setTimestamp();
-            if (queue.length) embed.addField('👉 Up Next', queue.map((track, index) => `**${index+1}.)** \`${track.info.author} - ${track.info.title}\``).join('\n'));
-        await interaction.reply({ embeds: [ embed ] });
+        if(dispatcher.queue.length > 10) {
+            const mapQueue = dispatcher.queue.map((track, index) => (
+                {
+                    queue_id: index+1,
+                    full_title: `${track.info.author} - ${track.info.title}`,
+                    author: track.info.author,
+                    title: track.info.title,
+                    url: track.info.uri,
+                    length: track.info.length
+                }
+            ));
+            const chunkedQueue = Queue.chunkify(mapQueue, 10);
+            const chunked = chunkedQueue.map((t, i) => ({ page: i, tracks: t }));
+            const pages = [];
+            
+            for (const q of chunked) {
+                pages.push(
+                    new MessageEmbed()
+                    .setAuthor(`Now Playing`, this.client.user.displayAvatarURL({ dynamic: true }))
+                    .setURL(dispatcher.current.info.uri)
+                    .setTitle(`**${dispatcher.current.info.title}**`)
+                    .setThumbnail(`https://img.youtube.com/vi/${dispatcher.current.info.identifier}/default.jpg`)
+                    .setDescription(`👉 **Queue List**\n\n${q.tracks.map(c => `**${c.queue_id}.)** [${c.title}](${c.url})`).join('\n')}`)
+                )
+            }
+            await interaction.deferReply()
+
+            new PagesBuilder(interaction)
+                .setColor(this.client.color)
+                .setPages(pages)
+                .setListenTimeout(60 * 1000)
+                .setListenEndMethod('delete')
+                .setDefaultButtons(Queue.pageButtonList)
+                .build();
+
+        } else {
+            await interaction.deferReply()
+            const embed = new MessageEmbed()
+                .setAuthor(`Now Playing`, this.client.user.displayAvatarURL({ dynamic: true }))
+                .setThumbnail(`https://img.youtube.com/vi/${dispatcher.current.info.identifier}/default.jpg`)
+                .setURL(dispatcher.current.info.uri)
+                .setColor(this.client.color)
+                .setTitle(`**${dispatcher.current.info.title}**`)
+                .addField(`⌛ Duration: `, `\`${RoxanneDispatcher.humanizeTime(dispatcher.current.info.length)}\``, true)
+                .addField(`🎵 Author: `, `\`${dispatcher.current.info.author}\``, true)
+                .setFooter(`• ${dispatcher.queue.length} total songs in queue`)
+                .setTimestamp();
+            await interaction.reply({ embeds: [ embed ] });
+        }
+        
     }
 }
 module.exports = Queue;
