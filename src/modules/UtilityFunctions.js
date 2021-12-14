ccbfeb283a3d194e48464c5ba57e7aecb6c478cf
txj-xyz <jfinelli3@gmail.com>
2021-12-14 07:19:28 -0700

    feat: Client utility modular exports

---
+++ src/modules/UtilityFunctions.js
@@ -0,0 +1,10 @@
+class UtilityFunctions {
+    constructor(client) {
+        this.util = client;
+        // Setup structure for module utility functions that can do various tasks for the client.util
+        return this;
+    }
+    
+}
+
+module.exports = UtilityFunctions;
