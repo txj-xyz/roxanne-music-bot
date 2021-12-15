e4a554e07dc9dd3f1ba25b465fada20dfdeeaad0
txj-xyz <jfinelli3@gmail.com>
2021-12-15 16:58:08 -0700

    feat: Search redesign

--- src/interactions/music/Search.js
+++ src/interactions/music/Search.js
@@ -37,24 +37,40 @@ class Search extends RoxanneInteraction {
         return { voice: true, dispatcher: false, channel: false };
     }
 
+    static humanizeTime(millisec) {
+        let seconds = (millisec / 1000).toFixed(0);
+        let minutes = Math.floor(seconds / 60);
+        let hours = '';
+        if (minutes > 59) {
+            hours = Math.floor(minutes / 60);
+            hours = hours >= 10 ? hours : '0' + hours;
+            minutes = minutes - hours * 60;
+            minutes = minutes >= 10 ? minutes : '0' + minutes;
+        }
+        seconds = Math.floor(seconds % 60);
+        seconds = seconds >= 10 ? seconds : '0' + seconds;
+        if (hours > 59) {
+            return 'Live! 🔴';
+        }
+        if (hours != '') {
+            return hours + ':' + minutes + ':' + seconds;
+        }
+        return minutes + ':' + seconds;
+    }
+
     async run({ interaction }) {
         const searchPageButtonList = [
             {
                 back: new MessageButton()
                     .setEmoji('👈')
                     .setLabel('Back')
-                    .setStyle('DANGER')
-            },
-            {
-                stop: new MessageButton()
-                    .setLabel('Cancel')
                     .setStyle('SECONDARY')
             },
             {
                 next: new MessageButton()
                     .setEmoji('👉')
                     .setLabel('Next')
-                    .setStyle('SUCCESS')
+                    .setStyle('SECONDARY')
             }
         ];
 
@@ -62,9 +78,8 @@ class Search extends RoxanneInteraction {
         const query = interaction.options.getString('query', true);
         if(query.includes('https://')) return await interaction.editReply('I can only search for words');
         const node = await this.client.shoukaku.getNode();
-
-        // Single search request for the query
         const search = await node.rest.resolve(query, 'youtube');
+        
         if (!search?.tracks.length)
             return interaction.editReply('I didn\'t find any search results on the query you provided!');
 
@@ -75,58 +90,63 @@ class Search extends RoxanneInteraction {
                 author: track.info.author,
                 title: track.info.title,
                 url: track.info.uri,
-                length: track.info.length
+                length: track.info.length,
+                identifier: track.info.identifier
             }
         ));
-        const chunkedSearch = Search.chunkify(mappedSearch, 10); // Split search into 10 results per page
+        const chunkedSearch = Search.chunkify(mappedSearch, 1); // Split search into 10 results per page
         const chunked = chunkedSearch.map((t, i) => ({ page: i, tracks: t }));
         const pages = [];
+        const c = this.client;
 
         for (const q of chunked) {
-            pages.push(
-                new MessageEmbed()
-                    .setDescription(`👉 **Search Results**\n\n${q.tracks.map(c => `**${c.search_id}.)** [${c.title}](${c.url})`).join('\n')}`)
-            );
+            q.tracks.map(c => {
+                pages.push(
+                    new MessageEmbed()
+                        // .setThumbnail(`https://img.youtube.com/vi/${c.identifier}/default.jpg`)
+                        .setImage(`https://img.youtube.com/vi/${c.identifier}/hqdefault.jpg`)
+                        .setURL(c.url)
+                        .setTitle(`**${c.title}**`)
+                        .addField('⌛ Duration: ', `\`${Search.humanizeTime(c.length)}\``, true)
+                        .addField('🎵 Author: ', `\`${c.author}\``, true)
+                        .setTimestamp()
+                );
+            }).join('\n')
         }
+
         let pageBuild = new PagesBuilder(interaction)
             .setColor(this.client.color)
             .setPages(pages)
             .setListenUsers(interaction.user.id)
-            .setListenTimeout(60 * 1000)
+            .setListenTimeout(15 * 1000)
             .setListenEndMethod('edit')
-            .setDefaultButtons(searchPageButtonList);
+            .setDefaultButtons(searchPageButtonList)
+            .addComponents(
+                [
+                    new MessageButton()
+                    .setCustomId('custom')
+                    .setEmoji('✅')
+                    .setLabel('Confirm')
+                    .setStyle('SUCCESS')
+                ]
+                
+            )
+            .setTriggers([
+                {
+                    name: 'custom',
+                    async callback(interactionCallback, button) {
+                        button.setDisabled(true)
+                        .setLabel('Added to Queue');
+                        const shoukakuTrack = search.tracks[pageBuild.currentPage - 1];
+                        const trackInformation = mappedSearch[pageBuild.currentPage - 1];
+                        const dispatcher = await c.queue.handle(interaction.guild, interaction.member, interaction.channel, node, shoukakuTrack);
+                            dispatcher?.play();
+                        return await interaction.followUp({content: `Adding **${trackInformation.full_title}** to the queue!`});
+                    }
+                }
+            ]);
 
         pageBuild.build();
-        const searchMessage = await interaction.channel.send('Please type a number from the search results!');
-        
-        interaction.channel.awaitMessages({filter: m => m.author.id == interaction.member.user.id, max: 1, time: 60 * 1000}).then(async collected => {
-            const isResponseNumber = Number(collected.first().content) ? true : false;
-        
-            if(isResponseNumber) {
-                const responseNumber = Number(collected.first().content);
-                const responseFinalResult = mappedSearch[responseNumber - 1];
-                const responseShoukakuTrack = search.tracks[responseNumber - 1];
-                
-                if(!responseShoukakuTrack){
-                    pageBuild.stopListen();
-                    await searchMessage.delete();
-                    return;
-                }
-
-
-                const dispatcher = await this.client.queue.handle(interaction.guild, interaction.member, interaction.channel, node, responseShoukakuTrack);
-                await interaction.channel.send(`Adding **${responseFinalResult.full_title}** to the queue!`);
-                dispatcher?.play();
-                await searchMessage.delete();
-                pageBuild.stopListen();
-                try {
-                    await collected.first().delete(); //Needs MANAGE_MESSAGES permission to not error
-                } catch (error) { return null; }
-            } else {
-                await searchMessage.delete();
-                pageBuild.stopListen();
-            }
-        }).catch(() => null);
     }
 }
 module.exports = Search;
