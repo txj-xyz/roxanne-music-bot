8a9fdf627f254c5d7e2d7b65ed0b5f96a7bcba86
txj-xyz <jfinelli3@gmail.com>
2021-12-20 23:57:16 +0000

    fix: reconstruct database handler

--- src/modules/DatabaseHandler.js
+++ src/modules/DatabaseHandler.js
@@ -4,15 +4,23 @@ class DatabaseHandler {
     constructor(client) {
         this.redis = null;
         this.redis = createClient({ url: 'redis://192.168.1.20:31181' });
-        this.redis.connect().catch((err) => {
-            this.redis = 'Not Connected';
-            client.logger.debug(this.constructor.name, err);
-            return this.redis;
-        });
-        client.logger.debug(
-            this.constructor.name,
-            'Loaded Database Handler without Error.'
-        );
+        this.redis
+            .connect()
+            .then(() => {
+                client.logger.debug(
+                    this.constructor.name,
+                    'Loaded Database Handler without Error.'
+                );
+            })
+            .catch((err) => {
+                this.redis = 'Not Connected';
+                client.logger.debug(
+                    this.constructor.name,
+                    `Failed to Connect to Database: ${err}`
+                );
+                return this.redis;
+            });
+
         return this;
     }
 }
