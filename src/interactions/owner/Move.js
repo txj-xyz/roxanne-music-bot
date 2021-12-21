8f3f2e482f9ecbfe596526aaa18cfe6a5f6d49de
txj-xyz <jfinelli3@gmail.com>
2021-12-21 02:29:23 +0000

    feat: move `VoiceChannel` of bot user `/move`

---
+++ src/interactions/owner/Move.js
@@ -0,0 +1,49 @@
+const RoxanneInteraction = require('../../abstract/RoxanneInteraction.js');
+
+class Move extends RoxanneInteraction {
+    get name() {
+        return 'move';
+    }
+
+    get description() {
+        return 'Changes voice channels!';
+    }
+
+    get permissions() {
+        return 'OWNER';
+    }
+
+    get playerCheck() {
+        return { voice: true, dispatcher: true, channel: false };
+    }
+
+    get options() {
+        return [];
+    }
+
+    async run({ interaction }) {
+        const botVoice =
+            (await interaction.guild.voiceStates.cache.get(
+                this.client.user.id
+            )) || null;
+        const userVoice =
+            (await interaction.guild.voiceStates.cache.get(
+                interaction.user.id
+            )) || null;
+        const botVoiceID = botVoice?.channelId;
+        const userVoiceID = userVoice?.channelId;
+        await interaction.deferReply({ ephemeral: true });
+        await interaction.editReply('Moving channels!');
+
+        botVoice.setChannel(userVoiceID).catch(async () => {
+            return await interaction.editReply(
+                "I'm unable to join your voice channel, I am missing permissions!"
+            );
+        });
+
+        await interaction.editReply(
+            `Moving from <#${botVoiceID}> to <#${userVoiceID}>`
+        );
+    }
+}
+module.exports = Move;
