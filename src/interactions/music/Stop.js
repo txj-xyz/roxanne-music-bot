1d791030a18beac159a69de43d9b6d9e3b50bad5
txj-xyz <jfinelli3@gmail.com>
2021-12-28 18:47:45 +0000

    fix<Stop>: manually handle leaving in 24/7 mode

--- src/interactions/music/Stop.js
+++ src/interactions/music/Stop.js
@@ -1,6 +1,6 @@
 const RoxanneInteraction = require('../../abstract/RoxanneInteraction.js');
 const Wait = require('util').promisify(setTimeout);
-const { foreverMode } = require('../config.json');
+const { foreverMode } = require('../../../config.json');
 
 class Stop extends RoxanneInteraction {
     get name() {
@@ -12,10 +12,25 @@ class Stop extends RoxanneInteraction {
     }
 
     get playerCheck() {
-        return { voice: true, dispatcher: true, channel: true };
+        return { voice: true, dispatcher: false, channel: true };
     }
 
     async run({ interaction, dispatcher }) {
+        // Manually handle the dispatcher checking here
+        if (!dispatcher) {
+            try {
+                await interaction.guild.voiceStates.cache.get(this.client.user.id).disconnect();
+                return await interaction.reply({
+                    content: 'I have left the voice channel!',
+                    ephemeral: false,
+                });
+            } catch (error) {
+                return await interaction.reply({
+                    content: 'Nothing is playing in this server!',
+                    ephemeral: true,
+                });
+            }
+        }
         await interaction.deferReply();
         dispatcher.queue.length = 0;
         dispatcher.repeat = 'off';
