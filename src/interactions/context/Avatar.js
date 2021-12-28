e479b903e60a19c4d5644dbc5e4157982be6b505
txj-xyz <jfinelli3@gmail.com>
2021-12-28 21:04:01 +0000

    cmon git it together

--- src/interactions/context/Avatar.js
+++ src/interactions/context/Avatar.js
@@ -15,11 +15,7 @@ class AvatarContext extends RoxanneContext {
         });
         const targetID = await interaction.guild.members.fetch(interaction.targetId);
         const avatar = targetID.user.displayAvatarURL({ dynamic: true });
-<<<<<<< HEAD
         const avatarEmbed = new MessageEmbed().setDescription(`Avatar of ${targetID.user.toString()}`);
-=======
-        const avatarEmbed = new MessageEmbed().setTitle(`Avatar of ${targetID.user.username}`);
->>>>>>> cc028cd5a7350e1fcbdffa40afb06f76bb4eb233
         await interaction.editReply({ embeds: [avatarEmbed], files: [new MessageAttachment(`${avatar}?size=4096`)] });
     }
 }
