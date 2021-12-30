4d1d5f6a195139cf438ea29fa2c8b5854fd782a8
txj-xyz <jfinelli3@gmail.com>
2021-12-30 15:06:47 +0000

    feat: `Prometheus` full integration support

--- src/events/Ready.js
+++ src/events/Ready.js
@@ -1,5 +1,5 @@
 const RoxanneEvent = require('../abstract/RoxanneEvent.js');
-
+const PrometheusCollector = require('../modules/PrometheusCollector.js');
 class Ready extends RoxanneEvent {
     get name() {
         return 'ready';
@@ -11,7 +11,9 @@ class Ready extends RoxanneEvent {
 
     async run() {
         this.client.logger.debug(`${this.client.user.username}`, `Ready! Serving ${this.client.guilds.cache.size} guild(s) with ${this.client.users.cache.size} user(s)`);
-        // await this.client.util.lava.requestToken(); //Initialize Spotify Client
+
+        // When client is ready, start gathering Prom stats
+        new PrometheusCollector(this.client).start();
         if (!this.interval) {
             await this.client.user.setActivity("What's the worst that could happen?!");
             const statuses = ['If you see this, dont look down. :)', 'Always got to make time for tea-time!', 'Burning... Love!', 'Going crazy, too much to do, too little time.'];
---
+++ src/modules/PrometheusCollector.js
@@ -0,0 +1,64 @@
+// const { token, webhookUrl, inviteURL, youtube_key, spotifyClientID, spotifySecret } = require('../../config.json');
+
+class PrometheusCollector {
+    constructor(client) {
+        this.client = client;
+
+        this._roxanne_guilds = new client.prom.client.Gauge({
+            name: 'roxanne_guilds',
+            help: 'total guilds across all shards',
+        });
+        this._roxanne_channels = new client.prom.client.Gauge({
+            name: 'roxanne_channels',
+            help: 'total channels across all shards',
+        });
+        this._roxanne_music_players = new client.prom.client.Gauge({
+            name: 'roxanne_music_players',
+            help: 'total lavalink players across all shards',
+        });
+        this._roxanne_user_count = new client.prom.client.Gauge({
+            name: 'roxanne_user_count',
+            help: 'total user count across all shards',
+        });
+        this._roxanne_commands_run = new client.prom.client.Gauge({
+            name: 'roxanne_commands_run',
+            help: 'total commands run on the bot',
+        });
+    }
+
+    async gather() {
+        const [guilds, channels, players] = await Promise.all([
+            this.client.shard.broadcastEval('this.guilds.cache.size'),
+            this.client.shard.broadcastEval('this.channels.cache.size'),
+            this.client.shard.broadcastEval('this.queue.size'),
+        ]);
+        return {
+            guilds: guilds.reduce((sum, count) => sum + count),
+            channels: channels.reduce((sum, count) => sum + count),
+            players: players.reduce((sum, count) => sum + count),
+            userCount: this.client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c),
+            commandsRun: this.client.commandsRun,
+        };
+    }
+
+    async start() {
+        // Set initial values from the first gather.
+        let i = await this.gather();
+        this._roxanne_guilds.set(i.guilds);
+        this._roxanne_channels.set(i.channels);
+        this._roxanne_music_players.set(i.players);
+        this._roxanne_user_count.set(i.userCount);
+        this._roxanne_commands_run.set(i.commandsRun);
+
+        setInterval(async () => {
+            i = await this.gather();
+            this._roxanne_guilds.set(i.guilds);
+            this._roxanne_channels.set(i.guilds);
+            this._roxanne_music_players.set(i.players);
+            this._roxanne_user_count.set(i.userCount);
+            this._roxanne_commands_run.set(i.commandsRun);
+        }, 5000);
+    }
+}
+
+module.exports = PrometheusCollector;
