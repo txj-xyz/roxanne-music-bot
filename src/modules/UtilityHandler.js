a7f7f19af49e77276ab332f5c325dcd7363eea3a
txj-xyz <jfinelli3@gmail.com>
2022-01-06 13:38:01 +0000

    fix: remake time handler, better formatting

--- src/modules/UtilityHandler.js
+++ src/modules/UtilityHandler.js
@@ -39,30 +39,15 @@ class UtilityHandler {
         return (hr != '' ? `${hr}:${min}:${sec}` : `${min}:${sec}`)
     }
 
-    convertMS(miliseconds, format) {
-        var days, hours, minutes, seconds, total_hours, total_minutes, total_seconds;
-
-        total_seconds = parseInt(Math.floor(miliseconds / 1000));
-        total_minutes = parseInt(Math.floor(total_seconds / 60));
-        total_hours = parseInt(Math.floor(total_minutes / 60));
-        days = parseInt(Math.floor(total_hours / 24));
-
-        seconds = parseInt(total_seconds % 60);
-        minutes = parseInt(total_minutes % 60);
-        hours = parseInt(total_hours % 24);
-
-        switch (format) {
-            case 's':
-                return total_seconds;
-            case 'm':
-                return total_minutes;
-            case 'h':
-                return total_hours;
-            case 'd':
-                return days;
-            default:
-                return { d: days, h: hours, m: minutes, s: seconds };
-        }
+    convertMS(ms) {
+        let seconds = (ms / 1000).toFixed(1),
+            minutes = (ms / (1000 * 60)).toFixed(1),
+            hours = (ms / (1000 * 60 * 60)).toFixed(1),
+            days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
+        if (seconds < 60) return seconds + ' Sec';
+        else if (minutes < 60) return minutes + ' Min';
+        else if (hours < 24) return hours + ' Hrs';
+        else return days + ' Days';
     }
 
     convertNumToInternational(number) {
