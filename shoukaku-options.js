3bebab3b8130c6ae69fb9f279cab755b691382a6
txj-xyz <jfinelli3@gmail.com>
2022-01-13 21:38:42 +0000

    init

--- shoukaku-options.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    moveOnDisconnect: true,
-    resumable: 'resumableRoxanne',
-    resumableTimeout: 45,
-    reconnectInterval: 2000,
-    reconnectTries: 5,
-    restTimeout: 10000,
-};
