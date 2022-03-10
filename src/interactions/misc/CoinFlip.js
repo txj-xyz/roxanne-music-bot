04be9d139e8365ba2a94d7e13c87cd9b4fd6bbd7
txj-xyz <jfinelli3@gmail.com>
2022-03-10 01:22:08 +0000

    feat: Add a `coinflip` command

---
+++ src/interactions/misc/CoinFlip.js
@@ -0,0 +1,28 @@
+const RoxanneInteraction = require('../../abstract/RoxanneInteraction.js');
+
+class CoinFlip extends RoxanneInteraction {
+    get name() {
+        return 'coinflip';
+    }
+
+    get description() {
+        return 'Basic coin flip that rolls heads or tails!';
+    }
+
+    static rolledResult() {
+        let flippedCoin;
+        const coinFlip = Math.floor(Math.random() * 2);
+        if (coinFlip == 0) {
+            flippedCoin = 'Heads';
+        } else {
+            flippedCoin = 'Tails';
+        }
+        return flippedCoin;
+    }
+
+    async run({ interaction }) {
+        const message = await interaction.deferReply();
+        await interaction.editReply(`**${CoinFlip.rolledResult().toString()}!**`);
+    }
+}
+module.exports = CoinFlip;
