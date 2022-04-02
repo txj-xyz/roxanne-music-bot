2449ee0eecc1e90a73f95f8d0b9ebc6a589bc52d
0x0b6e48 <jfinelli3@gmail.com>
2022-04-02 12:37:26 -0600

    tweak: keeping this format

--- src/modules/InteractionHandler.js
+++ src/modules/InteractionHandler.js
@@ -33,7 +33,7 @@ class InteractionHandler extends EventEmitter {
                 // this.client.logger.debug(this.constructor.name, `\tCommand '${Command.name}' loaded (@${Command.uid})`);
             }
         }
-        this.client.logger.log(
+        this.client.logger.log(this.constructor.name,
             this.constructor.name,
             `Loaded ${this.commands.size} interaction client command(s)`,
             this.commands.map((c) => c.name)
