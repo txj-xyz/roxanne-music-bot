e97abcdc16299fe1360304a7d035be6ae609ab8b
txj-xyz <jfinelli3@gmail.com>
2022-04-25 13:51:02 +0000

    dev: remove `CoinFlip` from Utility handler

--- src/modules/UtilityHandler.js
+++ src/modules/UtilityHandler.js
@@ -24,10 +24,6 @@ class UtilityHandler {
         return interaction.channel.messages.fetch(id).then((message) => message.delete());
     }
 
-    rollRandomInt16(number) {
-        return Math.floor(Math.random() * number);
-    }
-
     removeArrayIndex(array, indexID) {
         return array.filter((_, index) => index != indexID - 1);
     }
