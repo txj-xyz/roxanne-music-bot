fce4c9fed34968213a223cbcc4d543d229391a7e
txj-xyz <jfinelli3@gmail.com>
2022-04-25 14:37:24 +0000

    feat: terminal `color` support!

--- src/modules/RoxanneLogger.js
+++ src/modules/RoxanneLogger.js
@@ -1,13 +1,21 @@
 const { isMaster } = require('cluster');
 const { webhookUrl } = require('../../config.json');
 const { WebhookClient, MessageEmbed } = require('discord.js');
+const colors = require('colors');
+
+colors.setTheme({
+    info: 'green',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red',
+});
 
 class RoxanneLogger {
     constructor() {
         this.webhook = new WebhookClient({ url: webhookUrl });
         this.webhook.send('Health check initialized').catch((err) => {
             this.webhook = null;
-            console.log(`[ERROR] [Process ${process.pid}] [Cluster ${this.id}] Failed to initialize webhook, invalid URI, ${err.toString()}`);
+            console.log(`[ERROR] [Process ${process.pid}] [Cluster ${this.id}] Failed to initialize webhook, invalid URI, ${err.toString()}`.error);
         });
     }
 
@@ -27,21 +35,21 @@ class RoxanneLogger {
     }
 
     debug(handler, message) {
-        console.log(`[Process ${process.pid}] [Cluster ${this.id}] [${handler}] ${message}`);
+        console.log(`[Process ${process.pid}] [Cluster ${this.id}] [${handler}] ${message}`.debug);
     }
 
     log(message) {
         if (this.webhook) {
             this.webhook.send({ embeds: [this.logEmbed(message)] }).catch(console.error);
         }
-        console.log(`[Process ${process.pid}] [Cluster ${this.id}] [${message.constructor}] `, JSON.stringify(message, null, null));
+        console.log(`[Process ${process.pid}] [Cluster ${this.id}] [${message.constructor}] `.debug, colors.info(JSON.stringify(message, null, 1)));
     }
 
     error(error, message = 'Error detected, please check console') {
         if (this.webhook) {
             this.webhook.send({ embeds: [this.logEmbed(message)] }).catch(console.error);
         }
-        console.error(`[ERROR] [Process ${process.pid}] [Cluster ${this.id}] `, error);
+        console.error(`[ERROR] [Process ${process.pid}] [Cluster ${this.id}] `.error, colors.error(error));
     }
 }
 
