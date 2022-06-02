9ef64c5b212f7da9260e4e68cc23796601018bdc
txj-xyz <jfinelli3@gmail.com>
2022-06-02 03:21:12 +0000

    unused variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Constants, Intents, Util } = require('discord.js');
 const { ShardingManager } = require('kurasuta');
 const { join } = require('path');
-const { token, ipcSocket } = require('./config.json');
+const { token } = require('./config.json');
 
 const { GUILDS, GUILD_MEMBERS, GUILD_BANS, GUILD_VOICE_STATES, GUILD_MESSAGES, GUILD_MESSAGE_REACTIONS } = Intents.FLAGS;
 
@@ -16,19 +16,9 @@ const customClientOptions = {
 
 const sharderOptions = {
     clientOptions: Util.mergeDefault(Constants.DefaultOptions, customClientOptions),
-    // development: true,
-    // ipcSocket: ipcSocket || 9999,
     client: RoxanneClient,
     timeout: 90000,
     token,
 };
 
-// const ratelimitOptions = {
-//     handlerSweepInterval: 2 * 6000,
-//     hashInactiveTimeout: 4 * 6000,
-//     requestOffset: 500,
-// };
-
-// const azuma = new Azuma(join(__dirname, '/src/RoxanneBaseCluster.js'), sharderOptions, ratelimitOptions);
 new ShardingManager(join(__dirname, '/src/RoxanneBaseCluster.js'), sharderOptions).spawn();
-// azuma.spawn().catch(console.error);
