32c5a00529de80480ca956f439e82719a73f158a
txj-xyz <jfinelli3@gmail.com>
2022-06-03 19:43:14 +0000

    recode stats command to be cleaner and faster

--- src/interactions/info/Stats.js
+++ src/interactions/info/Stats.js
@@ -10,56 +10,34 @@ class Stats extends RoxanneInteraction {
         return 'My current info!';
     }
 
-    static convertBytes(bytes) {
-        const MB = Math.floor((bytes / 1024 / 1024) % 1000);
-        const GB = Math.floor(bytes / 1024 / 1024 / 1024);
-        if (MB >= 1000) return `${GB.toFixed(1)} GB`;
-        else return `${Math.round(MB)} MB`;
-    }
-
-    static updateStats(interaction, client, message, guilds, channels, memory, players) {
-        let obj = {
-            servers: Array.from(client.shoukaku.nodes.keys()).join(', '),
-            commandsRun: client.commandsRun,
-            uptime: client.util.convertMS(client.uptime),
-            ping: Math.round(message.createdTimestamp - interaction.createdTimestamp),
-            memory: Stats.convertBytes(memory.reduce((sum, memory) => sum + memory.rss, 0)),
-            players: players.reduce((sum, count) => sum + count),
-            channels: channels.reduce((sum, count) => sum + count),
-            userCount: client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c),
-            guilds: guilds.reduce((sum, count) => sum + count),
-        };
-        return obj;
-    }
-
     async run({ interaction }) {
-        const message = await interaction.deferReply({ fetchReply: true });
+        const pingTime = Date.now();
+        await interaction.deferReply();
         const [guilds, channels, memory, players] = await Promise.all([
             this.client.shard.broadcastEval('this.guilds.cache.size'),
             this.client.shard.broadcastEval('this.channels.cache.size'),
             this.client.shard.broadcastEval('process.memoryUsage()'),
             this.client.shard.broadcastEval('this.queue.size'),
         ]);
-        const statsQuery = Stats.updateStats(interaction, this.client, message, guilds, channels, memory, players);
 
         const embed = new MessageEmbed()
             .setColor(this.client.color)
             .setTitle('Status')
             .setDescription(
                 `\`\`\`ml\n
-Guilds      :: ${statsQuery.guilds}
-User Count  :: ${statsQuery.userCount}
-Channels    :: ${statsQuery.channels}
-Players     :: ${statsQuery.players}
-Memory      :: ${statsQuery.memory}
-Ping        :: ${statsQuery.ping} MS
-Uptime      :: ${statsQuery.uptime}
-CMDs Run    :: ${statsQuery.commandsRun}
-Music Nodes :: ${statsQuery.servers}
+Guilds      :: ${guilds.reduce((sum, count) => sum + count)}
+User Count  :: ${this.client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c)}
+Channels    :: ${channels.reduce((sum, count) => sum + count)}
+Players     :: ${players.reduce((sum, count) => sum + count)}
+Memory      :: ${this.client.util.convertBytes(memory.reduce((sum, memory) => sum + memory.rss, 0))}
+Ping        :: ${Math.round(Date.now() - pingTime)} MS
+Uptime      :: ${this.client.util.convertMS(client.uptime)}
+CMDs Run    :: ${this.client.commandsRun}
+Music Nodes :: ${Array.from(client.shoukaku.nodes.keys()).join(', ')}
 \`\`\``
             )
             .setTimestamp()
-            .setFooter(this.client.user.username, this.client.user.displayAvatarURL());
+            .setFooter({ text: this.client.user.username, iconURL: this.client.user.displayAvatarURL() });
         await interaction.editReply({ embeds: [embed], components: [] });
     }
 }
