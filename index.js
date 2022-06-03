cc3029d266f0ce234825cb26b078b128dc5b00f1
txj-xyz <jfinelli3@gmail.com>
2022-06-03 20:34:01 +0000

    revert: socket

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ const sharderOptions = {
     clientOptions: Util.mergeDefault(Constants.DefaultOptions, customClientOptions),
     client: RoxanneClient,
     timeout: 90000,
-    ipcSocket: 9998,
     token,
 };
 
