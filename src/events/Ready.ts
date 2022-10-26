54e8642fc57f0231378819b8e621dd469c5c24f8
txj-xyz <jfinelli3@gmail.com>
2022-10-26 04:30:12 +0000

    lol

--- src/events/Ready.ts
+++ src/events/Ready.ts
@@ -20,7 +20,7 @@ export default class Ready extends BotEvent {
 
     //test comment
     async run(client: Bot) {
-        this.client.logger.log({ message: `[${this.client.user?.username}] Ready! Serving ${this.client.guilds.cache.size} guild(s) with ${this.client.users.cache.size} user(s)` }, true);
+        client.logger.log({ message: `[${client.user?.username}] Ready! Serving ${this.client.guilds.cache.size} guild(s) with ${this.client.users.cache.size} user(s)` }, true);
         setInterval((): void => {
             const current = this.statuses.shift() ?? '';
             this.client.user?.setActivity(current);
