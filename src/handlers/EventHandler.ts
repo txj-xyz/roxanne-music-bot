879634d6db9b20267ac69c41a99e13a7887853b9
txj-xyz <jfinelli3@gmail.com>
2022-10-26 04:30:29 +0000

    unused variable

--- src/handlers/EventHandler.ts
+++ src/handlers/EventHandler.ts
@@ -10,7 +10,7 @@ export default class EventHandler {
     constructor(client: Bot) {
         this.client = client;
         this.built = false;
-        client.on('shardDisconnect', (event, id) => this.client.logger.log({ message: `Shard ${id} Shard Disconnecting`, handler: this.constructor.name }, true));
+        client.on('shardDisconnect', (_event, id) => this.client.logger.log({ message: `Shard ${id} Shard Disconnecting`, handler: this.constructor.name }, true));
         client.on('shardResumed', (id, rep) => this.client.logger.log({ message: `Shard ${id} Shard Resume | ${rep} events replayed`, handler: this.constructor.name }, true));
         client.on('shardReady', (id) => this.client.logger.log({ message: `Shard ${id} | Shard Ready`, handler: this.constructor.name, uid: `Internal Cluster` }, true));
     }
