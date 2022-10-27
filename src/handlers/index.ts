ded98e040f2329bb85836fb52e27e78b166f100a
txj-xyz <jfinelli3@gmail.com>
2022-10-27 19:06:05 +0000

    `ModuleHandler` exports

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,3 +2,4 @@ export { default as InteractionHandler } from './InteractionHandler';
 export { default as EventHandler } from './EventHandler';
 export { default as UtilityHandler } from './UtilityHandler';
 export { default as LoggingHandler } from './LoggingHandler';
+export { default as ModuleHandler } from './ModuleHandler';
