536791dfbf80d283af75a3e37866399648105958
txj-xyz <jfinelli3@gmail.com>
2022-11-01 20:34:48 +0000

    entire backend recode, full music integration

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,15 +1,26 @@
 import 'dotenv/config';
 import { Client, ClientOptions } from 'discord.js';
-import * as Handlers from './handlers';
+import { UtilityHandler, LoggingHandler, EventHandler, InteractionHandler } from './handlers';
+import { MusicBackend, MusicDispatcher, MusicQueue } from './music';
+
+// interface MusicModule {
+//     shoukaku: MusicBackend;
+//     queue: MusicQueue;
+//     dispatcher: MusicDispatcher;
+// }
 export default interface Bot {
     color: number;
-    util: Handlers.UtilityHandler;
+    util: UtilityHandler;
     quitting: boolean;
     location: string;
-    logger: Handlers.LoggingHandler;
-    interactions: Handlers.InteractionHandler;
-    events: Handlers.EventHandler;
-    modules: Handlers.ModuleHandler;
+    logger: LoggingHandler;
+    interactions: InteractionHandler;
+    events: EventHandler;
+    music: {
+        shoukaku: MusicBackend;
+        queue: MusicQueue;
+        dispatcher?: MusicDispatcher;
+    };
     commandsRun: number;
     login(): Promise<string>;
     exit(): void;
@@ -20,14 +31,17 @@ export default class Bot extends Client implements Bot {
         super(options);
         this.color = 0x7e686c;
         this.commandsRun = 0;
-        this.util = new Handlers.UtilityHandler(this);
+        this.util = new UtilityHandler(this);
         this.quitting = false;
         this.location = process.cwd();
-        this.logger = new Handlers.LoggingHandler();
-        this.interactions = new Handlers.InteractionHandler(this).build();
-        this.events = new Handlers.EventHandler(this).build();
-        this.modules = new Handlers.ModuleHandler(this).build();
+        this.logger = new LoggingHandler();
+        this.interactions = new InteractionHandler(this).build();
+        this.events = new EventHandler(this).build();
         ['beforeExit', 'SIGUSR1', 'SIGUSR2', 'SIGINT', 'SIGTERM'].map((event: string) => process.once(event, this.exit.bind(this)));
+        this.music = {
+            shoukaku: new MusicBackend(this),
+            queue: new MusicQueue(this),
+        };
     }
 
     async login(): Promise<string> {
