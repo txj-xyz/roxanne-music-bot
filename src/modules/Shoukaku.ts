e11bc9813ab3986665970683497b97e229a94fd3
txj-xyz <jfinelli3@gmail.com>
2022-11-01 16:44:08 +0000

    completed `Shoukaku` integration

--- src/modules/Shoukaku.ts
+++ src/modules/Shoukaku.ts
@@ -1,10 +1,11 @@
-import { Channel, Guild, GuildMember } from 'discord.js';
-import { Shoukaku, Connectors, Player, Node, Track } from 'shoukaku';
+import { Channel, EmbedBuilder, Guild, GuildMember } from 'discord.js';
+import { Shoukaku, Connectors, Player, Node, Track, WebSocketClosedEvent } from 'shoukaku';
 import Bot from '../Bot';
 import { BotModule } from '../handlers/ModuleHandler';
 
 export const enabled = true;
-export const name = "Music";
+export const name = 'Music';
+export default interface Music extends Shoukaku {}
 export interface MusicDispatcher {
     client: Bot;
     guild: Guild;
@@ -15,19 +16,24 @@ export interface MusicDispatcher {
     current: null | object;
     stopped: boolean;
     nowplaying: null;
+    playingEmbed(message: string | { name: string; track: Track }): EmbedBuilder;
+    handlePlayer(player: Player): void;
 }
+
+export interface MusicQueue {
+    client: Bot;
+    player?: Player;
+    dispatcher: MusicDispatcher;
+    handle(guild: Guild, member: GuildMember, channel: Channel, node: Node, track: Track, first: boolean): Promise<MusicDispatcher>;
+}
+
 export default class Music extends Shoukaku implements BotModule {
     constructor(client: Bot) {
         super(new Connectors.DiscordJS(client), client.util.config.lavalinkOptions, client.util.config.shoukakuOptions);
-        this.on('ready', (name, resumed) => {
-            client.logger.log({ handler: this.constructor.name, message: `Lavalink Node: ${name} is now ${resumed ? 'resumed' : 'connected'}` }, true);
-        });
-        this.on('error', (name, error) => {
-            client.logger.error({ handler: this.constructor.name, error: `Lavalink Node: ${name} had an error ${error.toString()}` }, true);
-        });
-        this.on('close', (name, code, reason) => {
-            client.logger.log({ handler: this.constructor.name, message: `Lavalink Node: ${name} closed with code ${code} ${reason ? `with reason: ${reason}` : ''}` }, true);
-        });
+        this.on('ready', (name, resumed) => client.logger.log({ handler: this.constructor.name, message: `Lavalink Node: ${name} is now ${resumed ? 'resumed' : 'connected'}` }, true));
+        this.on('error', (name, error) => client.logger.error({ handler: this.constructor.name, error: `Lavalink Node: ${name} had an error ${error.toString()}` }, true));
+        //prettier-ignore
+        this.on('close', (name, code, reason) => client.logger.log({ handler: this.constructor.name, message: `Lavalink Node: ${name} closed with code ${code} ${reason ? `with reason: ${reason}` : ''}` }, true));
         this.on('disconnect', (name, players, moved) => {
             client.logger.log(
                 {
@@ -48,25 +54,17 @@ export class MusicDispatcher {
         this.guild = guild;
         this.channel = channel;
         this.player = player;
-        this.queue = new Array<Track>
+        this.queue = new Array<Track>();
         this.repeat = 'off';
         this.current = null;
         this.stopped = false;
         this.nowplaying = null;
-
         this.player
-            .on('start', async () => {})
-            .on('end', async () => {})
-            .on('stuck', () => {});
-            // .on('closed', async (payload: WebSocketClosedEvent) => {});
+            .on('start', async () => client.logger.log({ handler: this.constructor.name, message: `Track Started` }, false))
+            .on('end', async () => client.logger.log({ handler: this.constructor.name, message: `Track Ended` }, false))
+            .on('stuck', () => client.logger.log({ handler: this.constructor.name, message: `Track Stuck` }, false))
+            .on('closed', async (payload: WebSocketClosedEvent) => client.logger.log({ handler: this.constructor.name, message: `Track closed with payload: ${payload.code}` }, false));
     }
-}
-
-export interface MusicQueue {
-    client: Bot;
-    player?: Player;
-    dispatcher: MusicDispatcher;
-    handle(guild: Guild, member: GuildMember, channel: Channel, node: Node, track: Track, first: boolean): Promise<MusicDispatcher>;
 }
 
 export class MusicQueue extends Map {
@@ -77,18 +75,14 @@ export class MusicQueue extends Map {
 
     public async handle(guild: Guild, member: GuildMember, channel: Channel, node: Node, track: Track, first: boolean): Promise<MusicDispatcher> {
         const existing = this.get(guild.id);
-
-        // TODO: HANDLE THE MISSING VOID CONNECTION CONNECTION FLAGS VIA BEFORE INTERACTION HANDLES IN `<Bot>.modules.<ModuleName>.queue.<Handle>`
         if (!existing && member.voice.channelId) {
-            this.player = await node.joinChannel({ guildId: guild.id, shardId: guild.shardId, channelId: member.voice.channelId, deaf: true })
+            this.player = await node.joinChannel({ guildId: guild.id, shardId: guild.shardId, channelId: member.voice.channelId, deaf: true });
             this.dispatcher = new MusicDispatcher(this.client, guild, channel, this.player);
             this.dispatcher.queue.push(track);
             this.set(guild.id, this.dispatcher);
             return this.dispatcher;
         }
-
         first ? existing.queue.unshift(track) : existing.queue.push(track);
-
         return this.dispatcher;
     }
 }
